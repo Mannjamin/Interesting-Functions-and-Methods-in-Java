304248302860b1310b858b95fb60936cb1bd45f3
Mannjamin <Mann.Ashley@outlook.com>
2017-11-20 12:25:46 +0000

    Update reverseString.java

--- Reverse-String-Method/reverseString.java
+++ Reverse-String-Method/reverseString.java
@@ -4,18 +4,18 @@ public class reverseString {
 		System.out.println("Reverse String:\n");
 		// Reverse String
 		String reversed = "!dekcirT neeb evah uoY !aH";
-		reverseStringMethod(reversed); // Calls and Inherits data from String
-										// "Reversed" above and uses it within
-										// the
-										// reverseString method.
+		reverseStringMethod(reversed); 	// Calls and Inherits data from String
+						// "Reversed" above and uses it within
+						// the
+						// reverseString method.
 	}
 
-	private static void reverseStringMethod(String b) { // Sub Method called in
-														// the main method with
-														// inherited B string.
-		String c = new StringBuilder(b).reverse().toString(); // New String Var
-																// C is String B
-																// in reverse.
+	private static void reverseStringMethod(String b) { 	// Sub Method called in
+								// the main method with
+								// inherited B string.
+		String c = new StringBuilder(b).reverse().toString(); 	// New String Var
+									// C is String B
+									// in reverse.
 		System.out.println(c); // Prints out String C.
 	}
 }
